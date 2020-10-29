cccf18d81366a012a13eb26983364b3bda1d217b
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-10-29 14:15:51 -0400

    tray icon working

---
+++ scripts/tray.js
@@ -0,0 +1,62 @@
+let tray;
+function createTray () {
+  // Create a tray icon
+  tray = new nw.Tray({
+    title: 'Tray',
+    tooltip: 'WuhdAppusDat',
+    icon: 'icon/WuhdAppusDat16.png'
+  });
+
+  // Give it a menu
+  let menu = new nw.Menu();
+
+  let itemChecked = true;
+
+  // Create an array of the items to be placed in the menu
+  let menuItems = [
+    {
+      type: 'normal',
+      label: 'Options',
+      click: function () {
+        global.optionsWindow.show();
+      }
+    },
+    {
+      type: 'normal',
+      label: 'Show Window',
+      click: function () {
+        global.appWindow.show();
+      }
+    },
+    {
+      type: 'normal',
+      label: 'Hide Window',
+      click: function () {
+        global.appWindow.hide();
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      type: 'normal',
+      label: 'Exit',
+      click: function () {
+        global.appWindow.close();
+      }
+    }
+  ];
+
+  // Append all menu items to the menu
+  menuItems.forEach(function (item) {
+    menu.append(new nw.MenuItem(item));
+  });
+
+  // Place the menu in the tray
+  tray.menu = menu;
+}
+
+function removeTray () {
+  tray.remove();
+  tray = null;
+}
