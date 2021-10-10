23f617d0ba851a1c81c39a5c18e9c82b3355c139
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2021-10-10 15:35:41 -0400

    Add HTTP-Vue-Loader

--- scripts/app-map.js
+++ scripts/app-map.js
@@ -2,53 +2,52 @@ const fs = require('fs');
 const settings = require('./scripts/settings.js').loadSettings();
 
 // eslint-disable-next-line no-unused-vars
-const app = Vue
-  .createApp({
-    data: function () {
-      return {
-        appMapVisible: false,
-        appMaps: []
-      };
-    },
-    methods: {
-      prepareAppMap: function () {
-        let appMap = settings.appMap;
-        if (!appMap) {
-          try {
-            appMap = settings.appMap || JSON.parse(fs.readFileSync('./app-map.json'));
-          } catch (err) {
-            appMap = {};
-          }
+const app = new Vue({
+  el: '#app-map-container',
+  data: function () {
+    return {
+      appMapVisible: false,
+      appMaps: []
+    };
+  },
+  methods: {
+    prepareAppMap: function () {
+      let appMap = settings.appMap;
+      if (!appMap) {
+        try {
+          appMap = settings.appMap || JSON.parse(fs.readFileSync('./app-map.json'));
+        } catch (err) {
+          appMap = {};
         }
-        let apps = [];
-        Object.keys(appMap).forEach(function (app) {
-          apps.push({
-            file: app,
-            alias: appMap[app]
-          });
-        });
+      }
+      let apps = [];
+      Object.keys(appMap).forEach(function (app) {
         apps.push({
-          file: '',
-          alias: ''
+          file: app,
+          alias: appMap[app]
         });
+      });
+      apps.push({
+        file: '',
+        alias: ''
+      });
 
-        this.appMaps = apps;
-      },
-      removeAppMap: function (index) {
-        this.appMaps.splice(index, 1);
-      }
+      this.appMaps = apps;
     },
-    computed: {
-      appMapToBeSaved: function () {
-        let appMap = {};
-        this.appMaps.forEach(function (app) {
-          appMap[app.file] = app.alias;
-        });
-        return appMap;
-      }
-    },
-    created: function () {
-      this.prepareAppMap();
+    removeAppMap: function (index) {
+      this.appMaps.splice(index, 1);
     }
-  })
-  .mount('#app-map-container');
+  },
+  computed: {
+    appMapToBeSaved: function () {
+      let appMap = {};
+      this.appMaps.forEach(function (app) {
+        appMap[app.file] = app.alias;
+      });
+      return appMap;
+    }
+  },
+  created: function () {
+    this.prepareAppMap();
+  }
+});
