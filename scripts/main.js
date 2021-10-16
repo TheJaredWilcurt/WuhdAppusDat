a196e65bbf1c1dd6d695b5bd13a9d1832e66d6ae
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2021-10-16 12:32:26 -0400

    Send background styles from options to main

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,12 @@ const fs = require('fs');
 const loadSettings = require('./scripts/settings.js').loadSettings;
 const {
   APP_TITLE,
-  DEFAULT_BACKGROUND_IMAGE,
   DEFAULT_INTERVAL,
   DEFAULT_VISIBLE_ON_ALL_WORKSPACES
 } = require('./scripts/global-constants.js');
 
 document.querySelector('title').innerText = APP_TITLE;
 
-const appName = document.getElementById('app-name');
-const background = document.getElementById('background');
 let interval;
 let settings;
 
@@ -58,44 +55,6 @@ function applySettings () {
     visibleOnAllWorkspacesValidated = DEFAULT_VISIBLE_ON_ALL_WORKSPACES;
   }
   nw.Window.get().setVisibleOnAllWorkspaces(visibleOnAllWorkspacesValidated);
-
-  // Background image
-  let backgroundImage = (settings && settings.background) || DEFAULT_BACKGROUND_IMAGE;
-
-  if (
-    settings &&
-    settings.background &&
-    settings.background !== 'images/leaves.png' &&
-    settings.background !== 'images/spikes.png' &&
-    settings.background !== 'images/bubbles.png'
-  ) {
-    backgroundImage = 'file://' + backgroundImage.split('\\').join('/');
-  }
-  background.style.backgroundImage = 'url("' + backgroundImage + '")';
-
-  let brightness;
-  let contrast;
-  let hueRotate;
-  let saturation;
-  if (typeof(settings.backgroundBrightness) === 'number') {
-    brightness = 'brightness(' + settings.backgroundBrightness + ')';
-  }
-  if (typeof(settings.backgroundContrast) === 'number') {
-    contrast = 'contrast(' + settings.backgroundContrast + ')';
-  }
-  if (typeof(settings.backgroundHueRotate) === 'number') {
-    hueRotate = 'hue-rotate(' + settings.backgroundHueRotate + 'deg)';
-  }
-  if (typeof(settings.backgroundSaturation) === 'number') {
-    saturation = 'saturate(' + settings.backgroundSaturation + ')';
-  }
-  let filters = [
-    brightness,
-    contrast,
-    hueRotate,
-    saturation
-  ].filter(Boolean).join(' ');
-  background.style.filter = filters || 'none';
 }
 
 function initialize () {
