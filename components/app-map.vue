b48eea0ee5486dd7ce2d0d845d5f1ff5708a9966
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2021-10-17 17:28:33 -0400

    styling

--- components/app-map.vue
+++ components/app-map.vue
@@ -6,10 +6,17 @@
       class="pill-form"
       :key="'app' + key"
     >
-      <input :value="key" class="pill-label">
-      <input :value="value" class="pill-content">
+      <input
+        :value="key"
+        class="pill-label"
+        @click="setKey(key)"
+      >
+      <input
+        :value="value"
+        class="pill-content last-pill-section"
+      >
       <button
-        class="pill-end last-pill-section"
+        class="pill-end"
         title="Remove"
         @click="removeAppMap(key)"
       >&times;</button>
@@ -21,6 +28,8 @@
 module.exports = {
   name: 'app-composition',
   methods: {
+    setKey: function () {
+    },
     removeApp: function () {
     }
   },
@@ -36,5 +45,19 @@ module.exports = {
 .app-map-container {
   width: 100%;
   text-align: center;
+}
+.app-map-container .pill-form {
+  border-radius: 0px;
+  margin: 5px 48px 0px 0px;
+  padding: 0px;
+  box-shadow: none;
+}
+.app-map-container .pill-label,
+.app-map-container .pill-content {
+  box-shadow: 2px 2px 5px rgba(136, 119, 91, 0.53);
+}
+.app-map-container .pill-end {
+  border-radius: 50%;
+  margin: 0px 0px 0px 4px;
 }
 </style>
