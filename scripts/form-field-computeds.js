1b7de8a232d8a01320ab6e10fec3dff72c2f9bcb
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2021-10-23 12:08:14 -0400

    Remove most files

--- scripts/form-field-computeds.js
+++
@@ -1,9 +0,0 @@
-const { generateUniqueId } = window.require('./scripts/helpers.js');
-
-const computed = {
-  id: function () {
-    return generateUniqueId();
-  }
-};
-
-module.exports = computed;
